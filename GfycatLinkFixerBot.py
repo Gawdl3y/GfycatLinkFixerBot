b22f17a11c7e2b7b389a42552602dd67a9c9b6fb
Schuyler Cebulskie <Gawdl3y@gmail.com>
2014-07-10 15:42:19 -0400

    Added proper logging, more exception handling

--- GfycatLinkFixerBot.py
+++ GfycatLinkFixerBot.py
@@ -1,18 +1,38 @@
 import configparser
+import logging
 import praw
-from praw.errors import RateLimitExceeded
 import re
 import time
+from praw.errors import APIException, RateLimitExceeded
+from requests.exceptions import HTTPError, ConnectionError, Timeout
+from socket import timeout
 from threading import Thread
 
 # Read the config file
 config = configparser.ConfigParser()
 config.read('GfycatLinkFixerBot.cfg')
 
+# Set up the logger for the console
+logger = logging.getLogger('GfycatLinkFixerBot')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(fmt = config.get('Logging', 'format'), datefmt = config.get('Logging', 'dateformat'))
+console_handler = logging.StreamHandler()
+console_handler.setLevel(int(config.get('Logging', 'consolelevel')))
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+# Add the file handler to the logger
+log_file = config.get('Logging', 'file').strip()
+if log_file:
+	file_handler = logging.FileHandler(log_file)
+	file_handler.setLevel(int(config.get('Logging', 'filelevel')))
+	file_handler.setFormatter(formatter)
+	logger.addHandler(file_handler)
+
 # Connect to Reddit
-r = praw.Reddit(user_agent = 'Gfycat Link Fixer Bot v1.1 by /u/Gawdl3y')
+r = praw.Reddit(user_agent = 'Gfycat Link Fixer Bot v1.2 by /u/Gawdl3y')
 r.login(config.get('Reddit', 'username'), config.get('Reddit', 'password'))
-print('Logged in to Reddit as ' + r.user.name)
+logger.info('Logged in to Reddit as ' + r.user.name)
 
 class Search(object):
 	pattern = re.compile('^https?://(zippy|fat|giant)\.gfycat\.com/([a-z]+)\.gif$', re.IGNORECASE)
@@ -23,9 +43,10 @@ class Search(object):
 		'[Banlist](http://www.reddit.com/r/GfycatLinkFixerBot/wiki/banlist) |\n'
 		'[Code](https://github.com/Gawdl3y/GfycatLinkFixerBot) |\n'
 		'[Subreddit](http://www.reddit.com/r/GfycatLinkFixerBot) |\n'
-		'Owner: /u/' + config.get('General', 'owner') + '  '
+		'Owner: /u/' + config.get('General', 'owner') + '  \n'
 		'Problems? Please message the owner or post in the subreddit.'
 	)
+	retry_sleep = config.get('General', 'retrytime')
 
 	def __init__(self, submission):
 		self.submission = submission
@@ -35,7 +56,7 @@ class Search(object):
 		if not self.has_posted():
 			self.post()
 		else:
-			print('Already posted in thread: ' + self.submission.permalink)
+			logger.info('Already posted in thread: ' + self.submission.permalink)
 
 	def has_posted(self):
 		for comment in self.submission.comments:
@@ -47,11 +68,27 @@ class Search(object):
 		while True:
 			try:
 				comment = self.submission.add_comment(Search.message.format(slug = self.match.group(2)))
-				print('Posted comment: ' + comment.permalink)
+				logger.info('Posted comment: ' + comment.permalink)
 				return
 			except RateLimitExceeded as e:
-				print('Rate limit exceeded; waiting {0} seconds to try again'.format(e.sleep_time))
+				logger.warning('Rate limit exceeded; retrying in ' + str(e.sleep_time) + ' seconds')
 				time.sleep(e.sleep_time)
+			except HTTPError as e:
+				if e.code == 403:
+					logger.error('Forbidden from posting comment: ' + self.submission.permalink)
+					return
+				else:
+					logger.warning('API error when posting comment; retrying in ' + str(self.retry_sleep) + ' seconds: ' + self.submission.permalink)
+					logger.warning(e)
+					time.sleep(self.retry_sleep)
+			except (ConnectionError, Timeout, timeout) as e:
+				logger.warning('Connection error when posting comment; retrying in ' + str(self.retry_sleep) + ' seconds: ' + self.submission.permalink)
+				logger.warning(e)
+				time.sleep(self.retry_sleep)
+			except APIException as e:
+				logger.warning('API error when posting comment; retrying in ' + str(self.retry_sleep) + ' seconds: ' + self.submission.permalink)
+				logger.warning(e)
+				time.sleep(self.retry_sleep)
 
 def main():
 	submissions = praw.helpers.submission_stream(r, 'all', limit = None)
